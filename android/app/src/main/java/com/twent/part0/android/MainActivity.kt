147202398a6079183bf3601d29edfd4f1d4edf55
TWEntCEO <twent1003@gmail.com>
2023-01-02 21:25:49 +0900

    0-ch04 complet

--- android/app/src/main/java/com/twent/part0/android/MainActivity.kt
+++ android/app/src/main/java/com/twent/part0/android/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.twent.part0.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- android/app/src/test/java/com/twent/part0/android/ExampleUnitTest.kt
+++ android/app/src/test/java/com/twent/part0/android/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.twent.part0.android
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
