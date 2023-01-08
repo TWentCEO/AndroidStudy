bfaf72216cdb96eddfdc9310c61f7f08896eb8ae
TWEntCEO <twent1003@gmail.com>
2023-01-08 22:41:52 +0900

    단위변환

---
+++ chpater3/app/src/main/java/com/twent/part1/chpater3/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.twent.part1.chpater3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.twent.part1.chpater3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(R.layout.activity_main)
+
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val inputEditTextView = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+        val swapImageButton = binding.swapImageButton
+
+        var inputNumber = 0
+        var cmToM = true
+
+        inputEditTextView.addTextChangedListener { text ->
+            Log.d("Main Activity", text.toString())
+            inputNumber = if (text.isNullOrEmpty()) {
+                0
+            } else {
+                text.toString().toInt()
+            }
+            if (cmToM) {
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+        swapImageButton.setOnClickListener {
+            cmToM = cmToM.not()
+            if (cmToM) {
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+    }
+}
