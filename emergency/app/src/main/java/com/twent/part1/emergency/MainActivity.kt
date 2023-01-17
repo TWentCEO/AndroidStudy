f43e20da44f30648aa8e93ff2fece4e80d54af61
TWEntCEO <twent1003@gmail.com>
2023-01-17 09:17:24 +0900

    add edit

---
+++ emergency/app/src/main/java/com/twent/part1/emergency/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.twent.part1.emergency
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.twent.part1.emergency.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goInputActivity.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            intent.putExtra("intentMessage", "응급의료정보")
+            startActivity(intent)
+        }
+    }
+}
